3a0faceab3e530eceba5c4eb581838e03e9fa894
Thomas Cicognani <github@tomtom.email>
2024-07-18 21:30:08 +0200

    refactor(user): improve User API quality
    Signed-off-by: Thomas Cicognani <github@tomtom.email>

--- UserAPI/src/main/java/fr/zhykos/demo/opt/user/User.java
+++ UserAPI/src/main/java/fr/zhykos/demo/opt/user/User.java
@@ -1,11 +1,4 @@
 package fr.zhykos.demo.opt.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class User {
-	private final String uuid;
-	private final String name;
+public record User(String uuid, String name) {
 }
