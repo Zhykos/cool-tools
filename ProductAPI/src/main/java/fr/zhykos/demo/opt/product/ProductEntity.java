a2b8936871418d504e97ea32f8ddbbd6359457a2
Thomas Cicognani <github@tomtom.email>
2024-07-18 21:42:23 +0200

    fix(products): rename table
    Signed-off-by: Thomas Cicognani <github@tomtom.email>

--- ProductAPI/src/main/java/fr/zhykos/demo/opt/product/ProductEntity.java
+++ ProductAPI/src/main/java/fr/zhykos/demo/opt/product/ProductEntity.java
@@ -3,8 +3,10 @@ package fr.zhykos.demo.opt.product;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "products")
 public record ProductEntity(
         @Id
         @GeneratedValue
