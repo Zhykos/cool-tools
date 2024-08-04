30ef732c68d942183b26a4004dc4c46f97014d25
Thomas Cicognani <github@tomtom.email>
2024-08-04 21:34:57 +0200

    refactor(order-api): clean commented code
    Signed-off-by: Thomas Cicognani <github@tomtom.email>

--- OrderAPI/main.go
+++ OrderAPI/main.go
@@ -7,12 +7,6 @@ import (
 
 func main() {
     router := gin.Default()
-
     router.POST("/order", handlers.CreateOrder)
-    //router.GET("/users", handlers.GetAllUsers)
-    //router.GET("/users/", handlers.GetUserByID)
-    //router.PATCH("/users/", handlers.UpdateUser)
-    //router.DELETE("/users/", handlers.DeleteUser)
-
     router.Run("0.0.0.0:9004")
 }
