dc8087604de489b37eba724274310fe5f724aecf
Thomas Cicognani <github@tomtom.email>
2024-08-04 21:41:59 +0200

    refactor(order-api): rename handlers into services
    Signed-off-by: Thomas Cicognani <github@tomtom.email>

--- OrderAPI/main.go
+++ OrderAPI/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
     "github.com/gin-gonic/gin"
-    "OrderAPI/handlers"
+    "OrderAPI/services"
 )
 
 func main() {
     router := gin.Default()
-    router.POST("/order", handlers.CreateOrder)
+    router.POST("/order", services.CreateOrder)
     router.Run("0.0.0.0:9004")
 }
