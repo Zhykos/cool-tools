1a96a0f7ffb77f331a0653aeeef72b0f4d42f993
Thomas Cicognani <github@tomtom.email>
2024-08-15 17:52:42 +0200

    feat(order-api): get user name to add it into the database
    Signed-off-by: Thomas Cicognani <github@tomtom.email>

---
+++ OrderAPI/README.md
@@ -0,0 +1,7 @@
+```bash
+go build
+```
+
+```bash
+MONGODB_URI=mongodb://root:password@localhost:9014 USER_API_URI=http://localhost:9001 ./OrderAPI
+```
--- OrderAPI/services/order.go
+++ OrderAPI/services/order.go
@@ -2,8 +2,15 @@ package services
 
 import (
     "context"
-    "net/http"
+    "encoding/json"
+    "errors"
+    "fmt"
     "github.com/gin-gonic/gin"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
+
     "OrderAPI/config"
     "OrderAPI/models"
 )
@@ -22,6 +29,9 @@ func CreateOrder(c *gin.Context) {
         return
     }
 
+    userName := GetUserName(order.UserID)
+    order.UserName = userName
+
     collection := client.Database("demo-opt-orders").Collection("orders")
     result, err := collection.InsertOne(context.Background(), order)
     if err != nil {
@@ -30,4 +40,49 @@ func CreateOrder(c *gin.Context) {
     }
 
     c.IndentedJSON(http.StatusCreated, result)
+}
+
+func GetUserName(userId string) (string) {
+    uri := os.Getenv("USER_API_URI")
+    if uri == "" {
+        fmt.Print("USER_API_URI is not set")
+        os.Exit(1)
+    }
+
+    response, err := http.Get(uri + "/user")
+
+    if err != nil {
+        fmt.Print(err.Error())
+        os.Exit(1)
+    }
+
+    allUsersRaw, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        log.Fatal(err)
+        os.Exit(1)
+    }
+
+    var users []models.User
+    err2 := json.Unmarshal(allUsersRaw, &users)
+    if err2 != nil {
+        log.Fatalf("Error unmarshaling JSON: %v", err2)
+        os.Exit(1)
+    }
+
+    user, err3 := searchByValue(users, userId)
+    if err3 != nil {
+        log.Fatalf("Cannot find user: %v", err3)
+        os.Exit(1)
+    }
+
+    return user.Name
+}
+
+func searchByValue(users []models.User, id string) (*models.User, error) {
+    for _, item := range users {
+        if item.UUID == id {
+            return &item, nil
+        }
+    }
+    return nil, errors.New("item not found")
 }
