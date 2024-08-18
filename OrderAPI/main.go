69fae8f44717388556f1afa9288ab2bd09178a82
Thomas Cicognani <github@tomtom.email>
2024-08-18 18:49:53 +0200

    refactor(order-api): call the service

--- OrderAPI/main.go
+++ OrderAPI/main.go
@@ -1,28 +1,26 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
 package main
 
 import (
 	"context"
-	"io"
-	"log"
-	"net/http"
-
+	"encoding/json"
+	"fmt"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/baggage"
 	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/propagation"
-
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
 	"go.opentelemetry.io/otel/trace"
+	"log"
+	"net/http"
+
+	"OrderAPI/models"
+    "OrderAPI/services"
 )
 
 func initTracer(ctx context.Context) (*sdktrace.TracerProvider, error) {
@@ -79,20 +77,53 @@ func main() {
 
 	uk := attribute.Key("username")
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+	createOrderHandler := func(w http.ResponseWriter, req *http.Request) {
 		ctx := req.Context()
 		span := trace.SpanFromContext(ctx)
 		bag := baggage.FromContext(ctx)
 		span.AddEvent("handling this...", trace.WithAttributes(uk.String(bag.Member("username").Value())))
 
-		_, _ = io.WriteString(w, "Hello, world!\n")
+        createOrder(w, req)
 	}
 
-	otelHandler := otelhttp.NewHandler(http.HandlerFunc(helloHandler), "Hello")
+	otelHandler := otelhttp.NewHandler(http.HandlerFunc(createOrderHandler), "Create Order")
 
-	http.Handle("/hello", otelHandler)
-	err = http.ListenAndServe(":7777", nil) //nolint:gosec // Ignoring G114: Use of net/http serve function that has no support for setting timeouts.
+	http.Handle("/order", otelHandler)
+	err = http.ListenAndServe("0.0.0.0:9004", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func createOrder(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost  {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
+
+    createOrderDTO := &models.Order{}
+    err := json.NewDecoder(r.Body).Decode(createOrderDTO)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    //logger.InfoContext(ctx, "create order:", createOrderDTO)
+    fmt.Println("create order:", createOrderDTO)
+    result, errStr, err2 := services.CreateOrder(*createOrderDTO)
+
+    if err2 != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        log.Fatalf("oops", err2)
+        return
+    }
+
+    if errStr != "" {
+        w.WriteHeader(http.StatusInternalServerError)
+        log.Fatalf(errStr)
+        return
+    }
+
+    json.NewEncoder(w).Encode(result)
+    w.WriteHeader(http.StatusCreated)
 }
