eb518d6d787d1883cfd699043b58cabfa681ee19
Thomas Cicognani <github@tomtom.email>
2024-08-18 18:00:29 +0200

    refactor(order-api): TEMP trying to auto instrument http

--- OrderAPI/main.go
+++ OrderAPI/main.go
@@ -1,128 +1,98 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package main
 
 import (
-    "context"
-    "encoding/json"
-    "errors"
-    "fmt"
-    "go.opentelemetry.io/contrib/bridges/otelslog"
-    "go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-    "go.opentelemetry.io/otel"
-    "log"
-    "net"
-    "net/http"
-    "os"
-    "os/signal"
-    "time"
+	"context"
+	"io"
+	"log"
+	"net/http"
 
-    "OrderAPI/models"
-    "OrderAPI/services"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/baggage"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/propagation"
+
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
+	"go.opentelemetry.io/otel/trace"
 )
 
-const name = "OrderAPI"
+func initTracer(ctx context.Context) (*sdktrace.TracerProvider, error) {
+	// Create stdout exporter to be able to retrieve
+	// the collected spans.
+	exporter, err := otlptracehttp.New(ctx)
+	if err != nil {
+		return nil, err
+	}
 
-var (
-	tracer = otel.Tracer(name)
-	logger = otelslog.NewLogger(name)
-)
+	// For the demonstration, use sdktrace.AlwaysSample sampler to sample all traces.
+	// In a production application, use sdktrace.ProbabilitySampler with a desired probability.
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(resource.NewWithAttributes(semconv.SchemaURL, semconv.ServiceName("ExampleService"))),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return tp, err
+}
+
+func initMeter() (*sdkmetric.MeterProvider, error) {
+	exp, err := stdoutmetric.New()
+	if err != nil {
+		return nil, err
+	}
+
+	mp := sdkmetric.NewMeterProvider(sdkmetric.WithReader(sdkmetric.NewPeriodicReader(exp)))
+	otel.SetMeterProvider(mp)
+	return mp, nil
+}
 
 func main() {
-    // Handle SIGINT (CTRL+C) gracefully.
-    ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-    defer stop()
+	tp, err := initTracer(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
+		}
+	}()
 
-    // Set up OpenTelemetry.
-    otelShutdown, err := setupOTelSDK(ctx)
-    if err != nil {
-        return
-    }
-    // Handle shutdown properly so nothing leaks.
-    defer func() {
-        err = errors.Join(err, otelShutdown(context.Background()))
-    }()
+	mp, err := initMeter()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := mp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down meter provider: %v", err)
+		}
+	}()
 
-    // Start HTTP server.
-    srv := &http.Server{
-        Addr:         "0.0.0.0:9004",
-        BaseContext:  func(_ net.Listener) context.Context { return ctx },
-        ReadTimeout:  time.Second,
-        WriteTimeout: 10 * time.Second,
-        Handler:      newHTTPHandler(),
-    }
-    srvErr := make(chan error, 1)
-    go func() {
-        srvErr <- srv.ListenAndServe()
-    }()
+	uk := attribute.Key("username")
 
-    // Wait for interruption.
-    select {
-    case err = <-srvErr:
-        // Error when starting HTTP server.
-        return
-    case <-ctx.Done():
-        // Wait for first CTRL+C.
-        // Stop receiving signal notifications as soon as possible.
-        stop()
-    }
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		ctx := req.Context()
+		span := trace.SpanFromContext(ctx)
+		bag := baggage.FromContext(ctx)
+		span.AddEvent("handling this...", trace.WithAttributes(uk.String(bag.Member("username").Value())))
 
-    // When Shutdown is called, ListenAndServe immediately returns ErrServerClosed.
-    err = srv.Shutdown(context.Background())
-    return
-}
+		_, _ = io.WriteString(w, "Hello, world!\n")
+	}
 
-func newHTTPHandler() http.Handler {
-    mux := http.NewServeMux()
+	otelHandler := otelhttp.NewHandler(http.HandlerFunc(helloHandler), "Hello")
 
-    // handleFunc is a replacement for mux.HandleFunc
-    // which enriches the handler's HTTP instrumentation with the pattern as the http.route.
-    handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
-        // Configure the "http.route" for the HTTP instrumentation.
-        handler := otelhttp.WithRouteTag(pattern, http.HandlerFunc(handlerFunc))
-        mux.Handle(pattern, handler)
-    }
-
-    // Register handlers.
-    createOrderRoute(handleFunc)
-
-    // Add HTTP instrumentation for the whole server.
-    handler := otelhttp.NewHandler(mux, "/")
-    return handler
-}
-
-func createOrderRoute(handleFunc func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request))) {
-    handleFunc("/order", func(w http.ResponseWriter, r *http.Request) {
-    	//ctx, span := tracer.Start(r.Context(), "createOrder")
-    	//defer span.End()
-
-        if r.Method != http.MethodPost  {
-            http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-            return
-        }
-
-        createOrderDTO := &models.Order{}
-        err := json.NewDecoder(r.Body).Decode(createOrderDTO)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusBadRequest)
-            return
-        }
-
-        //logger.InfoContext(ctx, "create order:", createOrderDTO)
-        fmt.Println("create order:", createOrderDTO)
-        result, errStr, err2 := services.CreateOrder(*createOrderDTO)
-
-        if err2 != nil {
-	        w.WriteHeader(http.StatusInternalServerError)
-	        log.Fatalf("oops", err2)
-	        return
-        }
-
-        if errStr != "" {
-            w.WriteHeader(http.StatusInternalServerError)
-            log.Fatalf(errStr)
-            return
-        }
-
-        json.NewEncoder(w).Encode(result)
-        w.WriteHeader(http.StatusCreated)
-    })
+	http.Handle("/hello", otelHandler)
+	err = http.ListenAndServe(":7777", nil) //nolint:gosec // Ignoring G114: Use of net/http serve function that has no support for setting timeouts.
+	if err != nil {
+		log.Fatal(err)
+	}
 }
