6e4edadc9047821b691edc87e8bd95031ca17676
Thomas Cicognani <thomas.cicognani@proton.me>
2024-09-07 13:33:08 +0200

    fix(product-api): CORS allow all origins

--- ProductAPI/src/main/java/fr/zhykos/cool/tools/product/ProductController.java
+++ ProductAPI/src/main/java/fr/zhykos/cool/tools/product/ProductController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class ProductController {
 	private final ProductService service;
 	private final ProductMapper mapper;
 
+	@CrossOrigin
 	@GetMapping(produces = "application/stream+json")
 	@Operation(summary = "List all products (flux)", description = "List all products in a reactive way")
 	@ApiResponse(responseCode = "200", description = "List of products (flux)")
