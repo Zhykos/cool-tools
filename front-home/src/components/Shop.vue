e008af8568794de4356297261c8dc68f168f1b02
Thomas Cicognani <thomas.cicognani@proton.me>
2024-09-07 12:40:38 +0200

    feat(front): component to create a new user

--- front-home/src/components/Shop.vue
+++ front-home/src/components/Shop.vue
@@ -1,7 +1,44 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import type { UserDTO } from "@/dto/UserDTO";
+import CreateUser from "./CreateUser.vue";
 import SelectedUser from "./SelectedUser.vue";
 import ShopItem from "./ShopItem.vue";
 import UsersList from "./UsersList.vue";
+
+const users = ref<UserDTO[]>([]);
+
+const userCreated = async (username: string) => {
+    try {
+        const res = await fetch("http://localhost:9001/user", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name: username }),
+        });
+
+        const data = await res.json();
+        users.value.push(data);
+        console.log("Created user:", data);
+    } catch (err) {
+        alert("Failed to create user");
+        console.error(err);
+    }
+};
+
+const initUsers = async () => {
+    try {
+        const res = await fetch("http://localhost:9001/user");
+        const data = await res.json();
+        users.value = data;
+    } catch (err) {
+        alert("Failed to get users");
+        console.error(err);
+    }
+};
+
+initUsers();
 </script>
 
 <template>
@@ -10,7 +47,11 @@ import UsersList from "./UsersList.vue";
         <SelectedUser />
     </ShopItem>
     <ShopItem>
+        <template #heading>Create a new user</template>
+        <CreateUser @user-created="userCreated" />
+    </ShopItem>
+    <ShopItem>
         <template #heading>Users list</template>
-        <UsersList />
+        <UsersList :users="users" />
     </ShopItem>
 </template>
--- front-home/src/components/UsersList.vue
+++ front-home/src/components/UsersList.vue
@@ -1,31 +1,21 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, defineProps } from "vue";
 import { UserDTO } from "@/dto/UserDTO";
 import { useUserStore } from "@/stores/shopStore";
 
+const props = defineProps<{ users: UserDTO[] }>();
 const users = ref<UserDTO[]>([]);
 const userWhoIsShopping = useUserStore();
 
-try {
-    fetch("http://localhost:9001/user").then((res) => {
-        res.json().then((data) => {
-            users.value = data;
-        });
-    });
-} catch (err) {
-    console.error(err);
-}
-
 function selectUser(user: UserDTO) {
     userWhoIsShopping.$patch({ user });
-    console.log(user.name);
 }
 </script>
 
 <template>
-    <p v-if="users.length === 0">No user: create a new one above</p>
+    <p v-if="props.users.length === 0">No user: create a new one above</p>
     <ul>
-        <li v-for="user in users" :key="user.uuid">
+        <li v-for="user in props.users" :key="user.uuid">
             <p>
                 <a href="#" @click="selectUser(user)">{{ user.name }}</a>
                 <i>({{ user.uuid }})</i>
