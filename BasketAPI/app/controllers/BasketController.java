718dc2568f5219cd9d97aad55efe9458df6552ef
Thomas Cicognani <thomas.cicognani@proton.me>
2024-12-01 19:01:17 +0100

    :bug: fix (basket-api): allow CORS

--- BasketAPI/app/controllers/BasketController.java
+++ BasketAPI/app/controllers/BasketController.java
@@ -10,7 +10,8 @@ public class BasketController extends Controller {
     private final List<BasketEntity> fakeDatabase = new ArrayList<>();
 
     public Result getBasket(String userId) {
-        var basketOpt = fakeDatabase.stream()
+        var basketOpt = fakeDatabase
+            .stream()
             .filter(basket -> basket.userId.equals(userId))
             .findFirst();
         if (basketOpt.isPresent()) {
@@ -26,6 +27,7 @@ public class BasketController extends Controller {
 
     public Result createBasket(Request request, String userId) {
         var body = request.body().parseJson(CreateBasketDTO.class);
+
         if (body.isPresent()) {
             var basket = new BasketEntity();
             Json.toJson(basket);
@@ -36,19 +38,26 @@ public class BasketController extends Controller {
             dto.productId = basket.productId;
             dto.userId = basket.userId;
             dto.basketId = basket.hashCode();
-            return created(Json.toJson(dto));
+
+            return created(Json.toJson(dto)).withHeader(
+                "Access-Control-Allow-Origin",
+                "*"
+            );
         }
+
         return badRequest();
     }
 
     public Result preflightCreateBasket(String userId) {
         System.out.println("Preflight request " + userId);
         return ok()
-                .withHeader("Access-Control-Allow-Origin", "*")
-                .withHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-                .withHeader("Access-Control-Allow-Headers", "*")
-                .withHeader("Access-Control-Allow-Credentials", "true")
-                .withHeader("Access-Control-Max-Age", "3600");
+            .withHeader("Access-Control-Allow-Origin", "*")
+            .withHeader(
+                "Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS"
+            )
+            .withHeader("Access-Control-Allow-Headers", "*")
+            .withHeader("Access-Control-Allow-Credentials", "true")
+            .withHeader("Access-Control-Max-Age", "3600");
     }
-
 }
