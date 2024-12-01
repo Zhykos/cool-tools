44edd5e25198445b52ef954c76fc7642e5f384fc
Thomas Cicognani <thomas.cicognani@proton.me>
2024-12-01 17:59:05 +0100

    :bug: chore (user-api): somehow Mapstruct is a pain in the :peach:
    so I removed it...

--- UserAPI/src/main/java/fr/zhykos/cool/tools/user/UserController.java
+++ UserAPI/src/main/java/fr/zhykos/cool/tools/user/UserController.java
@@ -18,27 +18,31 @@ import lombok.AllArgsConstructor;
 @Tag(name = "User", description = "User API")
 public class UserController {
 
-	private final UserService userService;
-	private final UserMapper mapper;
+    private final UserService userService;
+    private final UserMapper mapper;
 
-	@PostMapping
-	@CrossOrigin
-	@Operation(summary = "Create a user", description = "Create a user")
-	@Parameter(name = "dto", description = "User creation DTO", required = true)
-	@ApiResponse(responseCode = "201", description = "User created")
-	public ResponseEntity<UserDTO> createUser(@RequestBody final UserCreationDTO dto) {
-		final var createdUser = userService.createUser(dto.name());
-		return ResponseEntity.created(URI.create("http://localhost:9001/user/" + createdUser.uuid()))
-				.body(mapper.domainToDto(createdUser));
-	}
+    @PostMapping
+    @CrossOrigin
+    @Operation(summary = "Create a user", description = "Create a user")
+    @Parameter(name = "dto", description = "User creation DTO", required = true)
+    @ApiResponse(responseCode = "201", description = "User created")
+    public ResponseEntity<UserDTO> createUser(
+        @RequestBody final UserCreationDTO dto
+    ) {
+        final var createdUser = userService.createUser(dto.name());
+        return ResponseEntity.created(
+            URI.create("http://localhost:9001/user/" + createdUser.uuid())
+        ).body(mapper.domainToDto(createdUser));
+    }
 
-	@GetMapping
-	@CrossOrigin
-	@Operation(summary = "List all users", description = "List all users")
-	@ApiResponse(responseCode = "200", description = "List of users")
-	public ResponseEntity<List<UserDTO>> listUsers() {
-		final var users = userService.listUsers();
-		return ResponseEntity.ok(users.parallelStream().map(mapper::domainToDto).toList());
-	}
-
+    @GetMapping
+    @CrossOrigin
+    @Operation(summary = "List all users", description = "List all users")
+    @ApiResponse(responseCode = "200", description = "List of users")
+    public ResponseEntity<List<UserDTO>> listUsers() {
+        final var users = userService.listUsers();
+        return ResponseEntity.ok(
+                users.parallelStream().map(mapper::domainToDto).toList()
+        );
+    }
 }
